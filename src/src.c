a1449f5463c94e2b2fc895a13c73785c1e752725
Thuy Duong <vuhoang.thduong@gmail.com>
2023-05-29 00:06:22 +0200

    finish half of the first part

--- src/src.c
+++ src/src.c
@@ -29,3 +29,35 @@ int modpow(long a, long m, long n) {
   else if (m%2==0) return (modpow(a,m/2,n) * modpow(a,m/2,n)) % n; //m est pair
   else return (a * modpow(a,floor(m/2),n) * modpow(a,floor(m/2),n)) % n; //m est impair
 }
+
+/*TEST DE MILLER-RABIN*/
+int witness(long a, long b, long d, long p) {
+  long x = modpow(a,d,p);
+  if (x==1) return 0;
+  for(long i=0;i<b;i++) {
+    if (x==p-1) return 0;
+    x = modpow(x,2,p);
+  }
+  return 1;
+ }
+long rand_long(long low, long up) {
+  return rang() % (up-low+1) + low;
+}
+int is_prime_miller(long p, int k) {
+  if (p==2) return 1;
+  if (!(p&1) || p<=1) return 0; //verifie que p est impair et different de 1
+
+  //determine b et d
+  long b=0, d=p-1;
+  while(!(d&1)) { //tant que d pas impair
+    d/=2;
+    b++;
+  }
+  //generer k valeurs pour a, et teste si c'est un temoin : 
+  long a;
+  for (int i=0;i<k;i++) {
+    a = rand_long(2,p-1);
+    if (witness(a,b,d,p)) return 0;
+  }
+  return 1;
+}
--- src/src.h
+++ src/src.h
@@ -8,4 +8,9 @@ int is_prime_naive(long p);
 /*EXPONENTATION MODULAIRE RAPIDE*/
 long modpow_naive(long a, long m, long n);
 int modpow(long a, long m, long n);
+
+/*TEST DE MILLER-RABIN*/
+int witness(long a, long b, long d, long p);
+long rand_long(long low, long up);
+int is_prime_miller(long p, int k);
 #endif
